7cb591a04834d9a68452fd55b1a0f5a46a6f56ab
uchitesplavat <uchitesplavat@mail.ru>
2022-03-31 16:48:57 +0300

    UP_V_1

--- src/main.c
+++ src/main.c
@@ -1,5 +1,35 @@
-#include "mainhead.h"
+//#include "mainhead.h"
+#include <stdio.h>
+#include <stdlib.h>
+//#include "stb_image_resize.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
-main() {
-    
+
+int functionOpenfile();
+
+int main() {
+    //struct Stucture Pixel;
+    functionOpenfile();
+    return 0;
 }
+
+//int functionOpenfile(char path) {
+int functionOpenfile() {
+    int width, height, channels;
+    unsigned char *img = stbi_load("Cat03.jpg", &width, &height, &channels, 3);
+    if(img == NULL) {
+        printf("Error in loading the image\n");
+        exit(1);
+    }    
+    for (int i = 0; i <= width * height; i++) {
+        printf("%c", img[i]);
+    }
+    return 0;
+    //return img;
+}
+
+//void functionPrintascii() {
+//    for (int i = 0; i <= width * height)
+    //unsigned char *new_img = malloc(3 * sizeof(unsigned char) * width * height)
+//}
--- src/mainhead.h
+++ src/mainhead.h
@@ -2,3 +2,12 @@
 #include <stdlib.h>
 #include "stb_image_resize.h"
 #include "stb_image.h"
+#define STB_IMAGE_IMPLEMENTATION
+
+int functionOpenfile();
+
+//struct Structure {
+//    unsigned char r;
+//    unsigned char g;
+//    unsigned char b;
+//};
