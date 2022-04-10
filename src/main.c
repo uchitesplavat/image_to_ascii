2d2fb509708abb8e9eea47541fa1a34fd2322066
uchitesplavat <uchitesplavat@mail.ru>
2022-04-10 17:08:54 +0300

    UP_SOME_UPDATES_AND_MAKEFILE

--- src/main.c
+++ src/main.c
@@ -1,25 +1,4 @@
-//#include "mainhead.h"
-#include <stdio.h>
-#include <stdlib.h>
-//#include "stb_image_resize.h"
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "stb_image_resize.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-
-int functionPathToFile(char *path);
-int functionOpenfile(char *path, unsigned char *img, unsigned char *h_img, unsigned char *path_2, int *out_width);
-struct Structure {
-        unsigned char r;
-        unsigned char g;
-        unsigned char b;
-};
-unsigned char luminanceFromRGB(unsigned char r, unsigned char g, unsigned char b);
-unsigned char calc_ascii_char(struct Structure *Pixel, char *scale, unsigned int brightnessLevels, int i);
-void printingAscii(struct Structure *Pixel, int size, char *scale, unsigned int brightnessLevels, int *out_width);
-int functionWriteToStructure(unsigned char *img, int size, struct Structure *Pixel);
+#include "../src/mainhead.h"
 
 int main() {
     char *path = NULL;
@@ -31,20 +10,18 @@ int main() {
     unsigned char h_img[400000];
     char scale[] = "$@&B%8WM#ZO0QoahkbdpqwmLCJUYXIjft/\\|()1{}[]l?zcvunxr!<>i;:*-+~_,\"^`'. ";
     unsigned int brightnessLevels = (sizeof(scale) / sizeof(scale[0]) - 2);
-   // h_img = malloc(sizeof(unsigned char));
     int size, out_width = 0;
-    printf("Before_Path\n");
-    functionPathToFile(path); //reduce on 3 because of structure with 3 types;
-    printf("Before_Open_File\n");
+    //printf("Before_Path\n");
+    functionPathToFile(path);
+    //printf("Before_Open_File\n");
     size = functionOpenfile(path, img, h_img, path_2, &out_width);
     struct Structure Pixel[size];
-    printf("huy\n");
+    //printf("huy\n");
     functionWriteToStructure(h_img, size, Pixel);
-    printf("%d\n", h_img[0]);
+    //printf("%d\n", h_img[0]);
     printingAscii(Pixel, size, scale, brightnessLevels, &out_width);
-    printf("OUT_Width%d\n", out_width);
-    printf("OUT_Size%d\n", size);
-    //stbi_image_free(Pixel);
+    //printf("OUT_Width%d\n", out_width);
+    //printf("OUT_Size%d\n", size);
     return 0;
 }
 
@@ -57,30 +34,15 @@ int functionOpenfile(char *path, unsigned char *img, unsigned char *h_img, unsig
     }  
     out_w = width / 4;
     out_h = height / 9;
-    //h_img = realloc(h_img,width*height*3 * sizeof(unsigned char));
-    //for (int i = 0; i <= width * height; i++) {
-    //         h_img[i]=img[i];
-    //} 
-    //for (int i = 0; i <= 10; i++) {
-    //    printf("%d", img[i]);
-    //}
-    //printf("\n");
     path_2 = img;
     stbir_resize_uint8(img , width, height, 0,
                         path_2, out_w, out_h, 0, 3);
-
-           for (int i = 0; i <= out_w * out_h * 3; i++) {
-             h_img[i]=img[i];
-       } 
-    //for (int i = 0; i <= 10; i++) {
-    //    printf("%d", img[i]);
-    //}
-
-    //printf("\n");
+        for (int i = 0; i <= out_w * out_h * 3; i++) {
+            h_img[i]=img[i];
+        } 
     stbi_write_jpg("cat2.jpg", out_w, out_h, 3, path_2, 100);
     *out_width = out_w;
     return out_w * out_h;
-    //return img;
 }
 int functionPathToFile(char *path) {
         char *new_path = path;
@@ -101,16 +63,10 @@ int functionWriteToStructure(unsigned char *h_img, int size, struct Structure *P
         while (i < size * 3) {
                 Pixel[d].r = h_img[i];
                 Pixel[d].g = h_img[i + 1];
-                Pixel[d].b = h_img[i + 2];
-        //printf("%d", h_img[i]);
-        //printf("%d ", Pixel[i].r);
-        //printf("%d ", Pixel[i].g);
-        //printf("%d\n", Pixel[i].b);       
+                Pixel[d].b = h_img[i + 2];      
         d++;
         i+=3;
                         }
-//printf("%d\n", h_img[2]);
-
         return 0;
 }
 unsigned char luminanceFromRGB(unsigned char r, unsigned char g, unsigned char b) {
@@ -119,34 +75,18 @@ unsigned char luminanceFromRGB(unsigned char r, unsigned char g, unsigned char b
 
 unsigned char calc_ascii_char(struct Structure *Pixel, char *scale, unsigned int brightnessLevels, int i) {
     unsigned char ch = luminanceFromRGB(Pixel[i].r, Pixel[i].g, Pixel[i].b);
-    //printf("%d ", Pixel[i].g);
-    //printf("%d\n", Pixel[i].b);
-    //if (ch == 0) {
-    //   ch = 54;
-    //}
     int rescaled = ch * brightnessLevels / 256;
-    //printf("Scale %s\n", scale);
-    //printf("Brightness - rescaled = %d\n", brightnessLevels - rescaled);
     return scale[brightnessLevels - rescaled];
 }
-
-
-//stbir_resize_uint8(      input_pixels , in_w , in_h , 0,
-//                         output_pixels, out_w, out_h, 0, 3)
 
 void printingAscii(struct Structure *Pixel, int size, char *scale, unsigned int brightnessLevels, int *out_width) {
         printf("printingASCII\n");
         int g = *out_width;
         for (int i = 0; i <= size; i++) {
             printf("%c", calc_ascii_char(Pixel, scale, brightnessLevels, i));
-            //calc_ascii_char(Pixel, scale, brightnessLevels, i);
             if(i == g) {
                 printf("\n");
                 g += *out_width;
             }
         }
 }
-//void functionPri3ntascii() {
-//    for (int i = 0; i <= width * height)
-    //unsigned char *new_img = malloc(3 * sizeof(unsigned char) * width * height)
-//}
--- src/mainhead.h
+++ src/mainhead.h
@@ -1,13 +1,25 @@
+#ifndef MAINHEAD_H
+#define MAINHEAD_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "stb_image_resize.h"
-#include "stb_image.h"
 #define STB_IMAGE_IMPLEMENTATION
+#include "../src/stb_image.h"
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "../src/stb_image_resize.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "../src/stb_image_write.h"
 
-int functionOpenfile();
+int functionPathToFile(char *path);
+int functionOpenfile(char *path, unsigned char *img, unsigned char *h_img, unsigned char *path_2, int *out_width);
+struct Structure {
+        unsigned char r;
+        unsigned char g;
+        unsigned char b;
+};
+unsigned char luminanceFromRGB(unsigned char r, unsigned char g, unsigned char b);
+unsigned char calc_ascii_char(struct Structure *Pixel, char *scale, unsigned int brightnessLevels, int i);
+void printingAscii(struct Structure *Pixel, int size, char *scale, unsigned int brightnessLevels, int *out_width);
+int functionWriteToStructure(unsigned char *img, int size, struct Structure *Pixel);
 
-//struct Structure {
-//    unsigned char r;
-//    unsigned char g;
-//    unsigned char b;
-//};
+#endif  // MAINHEAD_H
